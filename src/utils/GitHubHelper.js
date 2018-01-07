856762cf846d1594f1448c21323407725a395714
EnixCoda <enixcoda@gmail.com>
2018-01-07 14:19:30 +0800

    refactor(GitHubHelper): abstract function 'request'

--- src/utils/GitHubHelper.js
+++ src/utils/GitHubHelper.js
@@ -1,25 +1,26 @@
 export const NOT_FOUND = 'Repo Not Found'
 
-async function getRepoMeta({ userName, repoName, accessToken }) {
+async function request(url, { accessToken } = {}) {
   const headers = {}
   if (accessToken) {
     headers.Authorization = `token ${accessToken}`
   }
-  const res = await fetch(`https://api.github.com/repos/${userName}/${repoName}`, { headers })
+  const res = await fetch(url, { headers })
   if (res.status === 200) return res.json()
   // for private repo, GitHub api also responses with 404 when unauthorized
-  throw new Error(NOT_FOUND)
+  if (res.status === 404) throw new Error(NOT_FOUND)
+  const content = await res.json()
+  throw new Error(content.message)
+}
+
+async function getRepoMeta({ userName, repoName, accessToken }) {
+  const url = `https://api.github.com/repos/${userName}/${repoName}`
+  return await request(url, { accessToken })
 }
 
 async function getTreeData({ userName, repoName, branchName, accessToken }) {
-  const headers = {}
-  if (accessToken) {
-    headers.Authorization = `token ${accessToken}`
-  }
-  return (await fetch(
-    `https://api.github.com/repos/${userName}/${repoName}/git/trees/${branchName}?recursive=1`,
-    { headers }
-  )).json()
+  const url = `https://api.github.com/repos/${userName}/${repoName}/git/trees/${branchName}?recursive=1`
+  return await request(url, { accessToken })
 }
 
 function getUrlForRedirect({ userName, repoName, branchName }, path) {
