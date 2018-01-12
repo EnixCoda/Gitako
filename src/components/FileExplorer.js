ad557477f968c548f9ad0a1ff61ddb6100f229bb
EnixCoda <enixcoda@gmail.com>
2018-01-12 18:31:34 +0800

    fix(FileExplorer): not scrolling to target when opening a file/folder directly

--- src/components/FileExplorer.js
+++ src/components/FileExplorer.js
@@ -28,6 +28,8 @@ export default class List extends preact.Component {
     const { root, nodes } = treeParser.parse(treeData, metaData)
     this.visibleNodesGenerator.plantTree(root, nodes)
     const currentPath = URLHelper.getCurrentPath()
+    this.tasksAfterRender.push(DOMHelper.focusFileExplorer)
+    this.tasksAfterRender.push(DOMHelper.focusSearchInput)
     if (currentPath.length) {
       const nodeExpandedTo = this.visibleNodesGenerator.expandTo(currentPath)
       this.visibleNodesGenerator.focusNode(nodeExpandedTo)
@@ -35,8 +37,6 @@ export default class List extends preact.Component {
       this.tasksAfterRender.push(() => DOMHelper.scrollToNodeElement(nodes.indexOf(nodeExpandedTo)))
     }
     this.updateVisibleNodes()
-    this.tasksAfterRender.push(DOMHelper.focusFileExplorer)
-    this.tasksAfterRender.push(DOMHelper.focusSearchInput)
   }
 
   componentDidMount() {
