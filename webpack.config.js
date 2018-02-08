02bbcff2e53e6a1d5b3437a6cbf9ec02d4134404
EnixCoda <enixcoda@gmail.com>
2018-02-08 13:15:50 +0800

    chore: update version number

--- webpack.config.js
+++ webpack.config.js
@@ -69,11 +69,13 @@ module.exports = {
         test: /\.svg$/,
         resourceQuery: /inline/,
         loader: ['url-loader'],
+        include: [srcPath],
       },
       {
         test: /\.svg$/,
         resourceQuery: /svgr/,
         loader: ['babel-loader', 'svgr/webpack'],
+        include: [srcPath],
       },
     ],
   },
