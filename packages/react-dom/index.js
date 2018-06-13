a94fd4cf71931bf02061e7f6ef20916251da2071
EnixCoda <enixcoda@gmail.com>
2018-06-13 22:53:18 +0800

    refactor: use React

--- packages/react-dom/index.js
+++
@@ -1,5 +0,0 @@
-import preact from 'preact'
-
-export default {
-  render: preact.render,
-}
--- src/components/Portal.js
+++ src/components/Portal.js
@@ -1,3 +1,15 @@
-import Portal from 'preact-portal'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Portal extends React.PureComponent {
+  render() {
+    const { into, children } = this.props
+    if (!(into instanceof window.Element)) return null
+    return ReactDOM.createPortal(
+      children,
+      into,
+    )
+  }
+}
 
 export default Portal
