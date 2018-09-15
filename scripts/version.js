cd5b9836cea5589d0663186eb4cd137d6f3c2d72
EnixCoda <enixcoda@gmail.com>
2018-09-15 22:00:28 +0800

    build: postversion command

--- scripts/version.js
+++ scripts/version.js
@@ -3,8 +3,8 @@ const path = require('path')
 
 const rootPath = path.resolve(__dirname, '../')
 const packageJSON = require(path.resolve(rootPath, 'package.json'))
-const manifestPath = path.resolve(rootPath, 'src/manifest.json');
+const manifestPath = path.resolve(rootPath, 'src/manifest.json')
 const manifest = require(manifestPath)
 
 manifest.version = packageJSON.version
-fs.writeFileSync(manifestPath, JSON.stringify(manifest), 'utf-8')
+fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, '  '), 'utf-8')
