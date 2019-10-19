f39403f959c926910a010bcfbc574b09965af0b6
EnixCoda <enixcoda@gmail.com>
2019-10-19 11:04:46 +0800

    build: push to remote before tell sentry

--- scripts/roll.sh
+++ scripts/roll.sh
@@ -8,6 +8,7 @@ echo "Got version $VERSION"
 
 # sentry
 yarn sentry-cli releases new "$VERSION"
+git push # make sure sentry can retrieve current commit on remote
 yarn sentry-cli releases set-commits $VERSION --commit "EnixCoda/Gitako@$GIT_SHA"
 yarn sentry-cli releases files "$VERSION" upload-sourcemaps dist --no-rewrite
 yarn sentry-cli releases finalize "$VERSION"
