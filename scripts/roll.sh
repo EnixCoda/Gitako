86e29adbe384290e57dd5a214ebba9af03329b59
EnixCoda <enixcoda@gmail.com>
2019-10-19 11:00:45 +0800

    build: associate commits

--- scripts/roll.sh
+++ scripts/roll.sh
@@ -2,14 +2,15 @@
 rm -rf dist
 yarn build
 
-GITAKO_VERSION=v$(node scripts/get-version.js)
-echo "Got version $GITAKO_VERSION"
+GIT_SHA=$(git rev-parse HEAD)
+VERSION=v$(node scripts/get-version.js)
+echo "Got version $VERSION"
 
 # sentry
-yarn sentry-cli releases new "$GITAKO_VERSION"
-yarn sentry-cli releases set-commits --auto $GITAKO_VERSION
-yarn sentry-cli releases files "$GITAKO_VERSION" upload-sourcemaps dist --no-rewrite
-yarn sentry-cli releases finalize "$GITAKO_VERSION"
+yarn sentry-cli releases new "$VERSION"
+yarn sentry-cli releases set-commits $VERSION --commit "EnixCoda/Gitako@$GIT_SHA"
+yarn sentry-cli releases files "$VERSION" upload-sourcemaps dist --no-rewrite
+yarn sentry-cli releases finalize "$VERSION"
 
 cd dist
 rm -f ./gitako.zip
