d23a543c3ce690b928db2e5a7957037c7360db1c
Enix <enixcoda@gmail.com>
2019-11-01 14:51:23 +0800

    chore: merge push commands

--- scripts/roll.sh
+++ scripts/roll.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 rm -rf dist
-git push
 yarn build
 
 GIT_SHA=$(git rev-parse HEAD)
@@ -8,8 +7,8 @@ VERSION=v$(node scripts/get-version.js)
 echo "Got version $VERSION"
 
 # sentry
-yarn sentry-cli releases new "$VERSION"
 git push # make sure sentry can retrieve current commit on remote
+yarn sentry-cli releases new "$VERSION"
 yarn sentry-cli releases set-commits "$VERSION" --auto
 yarn sentry-cli releases files "$VERSION" upload-sourcemaps dist --no-rewrite
 yarn sentry-cli releases finalize "$VERSION"
