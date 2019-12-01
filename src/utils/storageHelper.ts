effaca1f2cbaf570e6380c12cf9fdc5234bec65e
EnixCoda <enixcoda@gmail.com>
2019-12-01 23:03:07 +0800

    fix: protect stale extension context
    App would crash on storage manipulation when context is stale

--- src/utils/storageHelper.ts
+++ src/utils/storageHelper.ts
@@ -1,9 +1,13 @@
 const localStorage = browser.storage.local
 
-export function get(mapping: string[] | null): Promise<any> {
-  return localStorage.get(mapping || undefined)
+export function get(mapping: string[] | null): Promise<any> | any {
+  try {
+    return localStorage.get(mapping || undefined)
+  } catch (err) {}
 }
 
-export function set(value: any): Promise<void> {
-  return localStorage.set(value)
+export function set(value: any): Promise<void> | void {
+  try {
+    return localStorage.set(value)
+  } catch (err) {}
 }
