e58b4e2c07b1364c3675d1334f26360937ca1e9c
EnixCoda <enixcoda@gmail.com>
2020-04-02 23:46:30 +0800

    chore: remove legacy pjax

--- packages/pjax/lib/foreach-els.js
+++
@@ -1,9 +0,0 @@
-/* global HTMLCollection: true */
-
-module.exports = function(els, fn, context) {
-  if (els instanceof HTMLCollection || els instanceof NodeList || els instanceof Array) {
-    return Array.prototype.forEach.call(els, fn, context)
-  }
-  // assume simple DOM element
-  return fn.call(context, els)
-}
--- packages/pjax/lib/uniqueid.js
+++
@@ -1,8 +0,0 @@
-module.exports = (function() {
-  var counter = 0
-  return function() {
-    var id = ("pjax" + (new Date().getTime())) + "_" + counter
-    counter++
-    return id
-  }
-})()
