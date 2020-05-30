1a0513ad826a50119fdfb3e951b4e9dcfb121d91
EnixCoda <enixcoda@gmail.com>
2020-05-30 14:38:44 +0800

    feat: redirect root path for server

--- server/api/github.ts
+++ server/api/github.ts
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch'
-import { createCodeHandler } from '.'
+import { createCodeHandler } from './utils'
 
 const { GITHUB_OAUTH_CLIENT_ID, GITHUB_OAUTH_CLIENT_SECRET } = process.env
 
--- server/api/index.ts
+++ server/api/index.ts
@@ -1,36 +1,8 @@
 import { NowRequest, NowResponse } from '@now/node'
 
-export function createCodeHandler(oauthHandler: (code: string) => string | Promise<string>) {
-  return async function handleCode(request: NowRequest, response: NowResponse) {
-    const { code } = request.query
-    try {
-      setCORSHeaders(response)
-      if (!request.method || request.method.toLowerCase() !== 'post') {
-        return sendRejection(response, 405)
-      }
-      if (!code || typeof code !== 'string') {
-        return sendRejection(response, 403)
-      }
-      const accessToken = await oauthHandler(code)
-      writeJSON(response, { accessToken })
-      response.end()
-    } catch (err) {
-      return sendRejection(response, 400, err instanceof Error ? err.message : '')
-    }
-  }
-}
-
-function setCORSHeaders(response: NowResponse) {
-  response.setHeader('Access-Control-Allow-Origin', '*')
-  response.setHeader('Access-Control-Allow-Methods', 'POST')
-}
-
-export function sendRejection(response: NowResponse, status = 400, content?: string) {
-  response.writeHead(status)
-  response.end(content)
-}
-
-function writeJSON(response: NowResponse, data: unknown) {
-  response.setHeader('Content-Type', 'application/json')
-  response.write(JSON.stringify(data))
+export default function (request: NowRequest, response: NowResponse) {
+  response.writeHead(302, {
+    Location: 'https://github.com/EnixCoda/Gitako',
+  })
+  response.end()
 }
