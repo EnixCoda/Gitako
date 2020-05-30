d47604623849aa504452abebe07f9426caa7b2c7
EnixCoda <enixcoda@gmail.com>
2020-05-30 15:34:15 +0800

    build: deploy with vercel

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,6 @@ export const IN_PRODUCTION_MODE = process.env.NODE_ENV === 'production'
 
 export const GITHUB_OAUTH = {
   clientId: process.env.GITHUB_OAUTH_CLIENT_ID || '',
-  clientSecret: process.env.GITHUB_OAUTH_CLIENT_SECRET || '',
 }
 
 export const GITEE_OAUTH = {
