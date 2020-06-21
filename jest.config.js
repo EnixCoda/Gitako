2b86291ecddd672448db364730b292ecde9993af
EnixCoda <enixcoda@gmail.com>
2020-06-21 23:19:07 +0800

    test: longer timeout

--- jest.config.js
+++ jest.config.js
@@ -158,6 +158,8 @@ module.exports = {
   // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
   // testURL: "http://localhost",
 
+  testTimeout: 20000,
+
   // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
   // timers: "real",
 
