de5b67bb2fbdb6db5fef25361d216603209b87df
EnixCoda <enixcoda@gmail.com>
2020-10-28 16:25:45 +0800

    test: add file list render test cases

---
+++ __tests__/cases/pull-request-page.gitako.ts
@@ -0,0 +1,13 @@
+import { expectToFind } from '../utils'
+
+describe(`in Gitako project page`, () => {
+  beforeAll(() => page.goto('https://github.com/EnixCoda/Gitako/pull/71'))
+
+  it('should render Gitako', async () => {
+    await expectToFind('.gitako-side-bar .gitako-position-wrapper')
+  })
+
+  it('should render file list', async () => {
+    await expectToFind('.gitako-side-bar .files .node-item')
+  })
+})
--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -1,7 +1,27 @@
 export async function expectToFind(selector: string) {
-  await expect(await page.waitForSelector(selector)).not.toBeNull()
+  await expect(page.waitForSelector(selector)).resolves.not.toBeNull()
 }
 
 export async function expectToNotFind(selector: string) {
   await expect(page.waitForSelector(selector, { timeout: 1000 })).rejects.toThrow()
+}
+
+export function sleep(timeout: number) {
+  return new Promise(resolve => setTimeout(resolve, timeout))
+}
+
+export async function scroll({
+  totalDistance,
+  step = 1,
+  duration = 500,
+}: {
+  totalDistance: number
+  step?: number
+  duration?: number
+}) {
+  let distance = 0
+  while ((distance += step) < totalDistance) {
+    await (page.mouse as any).wheel({ deltaY: step })
+    await sleep((duration * step) / totalDistance)
+  }
 }
