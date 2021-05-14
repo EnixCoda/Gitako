a6884b981d8ebe55eed36decfe87ff16cf0f2720
EnixCoda <enixcoda@gmail.com>
2021-05-14 09:42:37 +0800

    fix: skip access token related state if not necessary

--- src/components/OAuthWrapper.tsx
+++ src/components/OAuthWrapper.tsx
@@ -13,8 +13,15 @@ export function OAuthWrapper({ children }: React.PropsWithChildren<{}>) {
   const running = useGetAccessToken()
   const $state = useLoadedContext(SideBarStateContext)
 
+  const firstRenderRef = React.useRef(true)
   React.useEffect(() => {
-    $state.onChange(running ? 'getting-access-token' : 'after-getting-access-token')
+    if (running) {
+      $state.onChange('getting-access-token')
+    } else if (firstRenderRef.current) {
+      firstRenderRef.current = false
+    } else {
+      $state.onChange('after-getting-access-token')
+    }
   }, [running])
 
   // block children rendering on the first render if setting token
