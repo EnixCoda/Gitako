7fadb5fd576e007abcc83d18ebde648e723adeee
EnixCoda <enixcoda@gmail.com>
2022-05-12 23:25:00 +0800

    build: upgrade react

---
+++ src/react-override.d.ts
@@ -0,0 +1,10 @@
+// Patch the removed `children` prop
+declare namespace React {
+  interface FunctionComponent<P = {}> {
+    (props: PropsWithChildren<P>, context?: any): ReactElement<any, any> | null;
+    propTypes?: WeakValidationMap<P> | undefined;
+    contextTypes?: ValidationMap<any> | undefined;
+    defaultProps?: Partial<P> | undefined;
+    displayName?: string | undefined;
+  }
+}
