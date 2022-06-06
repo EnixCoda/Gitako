6c671da52022c8fa4cb496b8b62cd8c54715890c
EnixCoda <enixcoda@gmail.com>
2022-06-06 20:51:54 +0800

    test: add unit test

--- jest.config.js
+++ jest.config.js
@@ -133,7 +133,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: 'node',
+  testEnvironment: 'jsdom',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
